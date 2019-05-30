dd605137544b66fb2455fc589778cd2d9b45a443
dadang <dadang@javan.co.id>
2019-05-31 06:07:16 +0700

    Initial commit

---
+++ src/main/java/id/co/javan/berbageek/BerbageekApplication.java
@@ -0,0 +1,13 @@
+package id.co.javan.berbageek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerbageekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerbageekApplication.class, args);
+	}
+
+}
